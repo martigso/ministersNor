d1ec89f25d74228c6acd33b6125e112073976aa2
martigso <martin_soyland@hotmail.com>
2016-04-30 16:49:59 +0200

    A great deal of modification

--- R/st.lague.R
+++ R/st.lague.R
@@ -27,59 +27,13 @@ st.lague<-function(seats, n.posts, n.parties){
   
   seats_now = seats # seats in parliament
   
-  
+  dividers = cbind(seq(1, 100, 1), seq(3, 202, 2))
+  dividers[1:2,1]
   while(n.posts > sum(posts_now)){
-    seats_now = seats # it is important to not write over the seats vector, as it is reset after each loop
-    
-    seats_now = ifelse(posts_now==1, seats_now/3, seats_now)
-    seats_now = ifelse(posts_now==2, seats_now/5, seats_now)
-    seats_now = ifelse(posts_now==3, seats_now/7, seats_now)
-    seats_now = ifelse(posts_now==4, seats_now/9, seats_now)
-    seats_now = ifelse(posts_now==5, seats_now/11, seats_now)
-    seats_now = ifelse(posts_now==6, seats_now/13, seats_now)
-    seats_now = ifelse(posts_now==7, seats_now/15, seats_now)
-    seats_now = ifelse(posts_now==8, seats_now/17, seats_now)
-    seats_now = ifelse(posts_now==9, seats_now/19, seats_now)
-    seats_now = ifelse(posts_now==10, seats_now/21, seats_now)
-    seats_now = ifelse(posts_now==11, seats_now/23, seats_now)
-    seats_now = ifelse(posts_now==12, seats_now/25, seats_now)
-    seats_now = ifelse(posts_now==13, seats_now/27, seats_now)
-    seats_now = ifelse(posts_now==14, seats_now/29, seats_now)
-    seats_now = ifelse(posts_now==15, seats_now/31, seats_now)
-    seats_now = ifelse(posts_now==16, seats_now/33, seats_now)
-    seats_now = ifelse(posts_now==17, seats_now/35, seats_now)
-    seats_now = ifelse(posts_now==18, seats_now/37, seats_now)
-    seats_now = ifelse(posts_now==19, seats_now/39, seats_now)
-    seats_now = ifelse(posts_now==20, seats_now/41, seats_now)
-    seats_now = ifelse(posts_now==21, seats_now/43, seats_now)
-    seats_now = ifelse(posts_now==22, seats_now/45, seats_now)
-    seats_now = ifelse(posts_now==23, seats_now/47, seats_now)
-    seats_now = ifelse(posts_now==24, seats_now/49, seats_now)
-    seats_now = ifelse(posts_now==25, seats_now/51, seats_now)
-    seats_now = ifelse(posts_now==26, seats_now/53, seats_now)
-    seats_now = ifelse(posts_now==27, seats_now/55, seats_now)
-    seats_now = ifelse(posts_now==28, seats_now/57, seats_now)
-    seats_now = ifelse(posts_now==29, seats_now/59, seats_now)
-    seats_now = ifelse(posts_now==30, seats_now/61, seats_now)
-    seats_now = ifelse(posts_now==31, seats_now/63, seats_now)
-    seats_now = ifelse(posts_now==32, seats_now/65, seats_now)
-    seats_now = ifelse(posts_now==33, seats_now/67, seats_now)
-    seats_now = ifelse(posts_now==34, seats_now/69, seats_now)
-    seats_now = ifelse(posts_now==35, seats_now/71, seats_now)
-    seats_now = ifelse(posts_now==36, seats_now/73, seats_now)
-    seats_now = ifelse(posts_now==37, seats_now/75, seats_now)
-    seats_now = ifelse(posts_now==38, seats_now/77, seats_now)
-    seats_now = ifelse(posts_now==39, seats_now/79, seats_now)
-    seats_now = ifelse(posts_now==40, seats_now/81, seats_now)
-    seats_now = ifelse(posts_now==41, seats_now/83, seats_now)
-    seats_now = ifelse(posts_now==42, seats_now/85, seats_now)
-    seats_now = ifelse(posts_now==43, seats_now/87, seats_now)
-    seats_now = ifelse(posts_now==44, seats_now/89, seats_now)
-    seats_now = ifelse(posts_now==45, seats_now/91, seats_now)
-    seats_now = ifelse(posts_now==46, seats_now/93, seats_now)
-    seats_now = ifelse(posts_now==47, seats_now/95, seats_now)
-    
-    
+    seats_now = seats # it is important to not write over the seats vector, as it is reset after each loop 
+      for(i in 1:nrow(dividers)){
+        seats_now = ifelse(posts_now==dividers[i,1], seats_now/(dividers[i,2]), seats_now)
+      }
     posts_now = ifelse(seats_now==max(seats_now), posts_now+1, posts_now)
   }
   
